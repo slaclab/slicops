d4751e13119de2a6eb41d5ae8e34d0fa387d589f
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-12-31 18:30:07 +0000

    fmt

--- slicops/__init__.py
+++ slicops/__init__.py
@@ -3,6 +3,7 @@
 :copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
 :license: http://github.com/slaclab/slactwin/LICENSE
 """
+
 import pkg_resources
 
 try:
