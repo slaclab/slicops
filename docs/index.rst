60593217297174c3d318898cdbc66f4dfa111409
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-12-31 18:08:20 +0000

    Fix #5 Python project files

---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+Welcome to slicops
+=================
+
+slicops
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+.. toctree::
+   :maxdepth: 2
+
+   slicops
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ slicops/slicops_console.py
@@ -0,0 +1,18 @@
+"""Front-end command line for :mod:`slicops`.
+
+See :mod:`pykern.pkcli` for how this module is used.
+
+:copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slactwin/LICENSE
+"""
+
+import pykern.pkcli
+import sys
+
+
+def main():
+    return pykern.pkcli.main("slicops")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
