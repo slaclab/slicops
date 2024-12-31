fda2889b41dae6b4b6cd589a1ba25d37ee57528d
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-12-31 11:32:53 -0700

    Fix #5 Python project files (#6)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# SlicOps
-Demo repository for e-to-e HLA app.
+### slicops
+
+slicops
+
+Learn more at https://github.com/slaclab/slicops.
+
+#### License
+
+License: http://github.com/slaclab/slactwin/LICENSE
+
+Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
---
+++ slicops/__init__.py
@@ -0,0 +1,13 @@
+""":mod:`slicops` package
+
+:copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slactwin/LICENSE
+"""
+
+import pkg_resources
+
+try:
+    # We only have a version once the package is installed.
+    __version__ = pkg_resources.get_distribution("slicops").version
+except pkg_resources.DistributionNotFound:
+    pass
