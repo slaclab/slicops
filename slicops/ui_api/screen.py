5acd9f8fe9069281df6d614e4c07588cb247f9fb
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-01-25 01:29:31 +0000

    ckp

--- slicops/ui_api/screen.py
+++ slicops/ui_api/screen.py
@@ -4,15 +4,14 @@
 :license: http://github.com/slaclab/slicops/LICENSE
 """
 
-from lcls_tools.common.controls.pyepics.utils import PV, PVInvalidError
 from pykern import pkconfig
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdlog, pkdp
 import asyncio
 import lcls_tools.common.data.fitting_tool
-import lcls_tools.common.devices.reader
 import numpy
 import random
+import slicops.device
 import slicops.quest
 import slicops.ui_schema
 import time
@@ -20,34 +19,6 @@ import time
 _NAME = "screen"
 
 _cfg = None
-
-# TODO(pjm): needed to monkey path reader._device data to add a dev camera
-_old_device_data = lcls_tools.common.devices.reader._device_data
-
-
-def _monkey_patch_device_data(area=None, device_type=None, name=None):
-    res = _old_device_data(area, device_type, name)
-    res["screens"][_cfg.dev.camera_name] = {
-        "controls_information": {
-            "PVs": {
-                "image": "13SIM1:image1:ArrayData",
-                "n_col": "13SIM1:cam1:ArraySizeX_RBV",
-                "n_row": "13SIM1:cam1:ArraySizeY_RBV",
-                "n_bits": "13SIM1:cam1:N_OF_BITS",
-                "resolution": "13SIM1:cam1:RESOLUTION",
-            },
-            "control_name": "13SIM1:cam1",
-        },
-        "metadata": {
-            "area": "GUNB",
-            "beam_path": ["SC_DIAG0", "SC_HXR", "SC_SXR"],
-            "sum_l_meters": 0.0,
-        },
-    }
-    return res
-
-
-lcls_tools.common.devices.reader._device_data = _monkey_patch_device_data
 
 
 class API(slicops.quest.API):
@@ -203,75 +174,43 @@ class API(slicops.quest.API):
         s = self.session.ui_schema
         ux.camera.valid_values = s.cameras_for_beam_path(_cfg.dev.beam_path)
         ux.pv.value = s.camera_pv(_cfg.dev.beam_path, _cfg.dev.camera_name)
-        self.session.screen_device = _ScreenDevice(self.session.ui_schema)
+        self.session.screen_device = _ScreenDevice()
         # TODO(pjm): only get this value if the selected camera supports Gain
-        ux.camera_gain.value = self.session.screen_device.get_pv("Gain")
+        ux.camera_gain.value = self.session.screen_device.device.get("Gain")
         return ux
 
 
 class _ScreenDevice:
     """Screen device interaction. All EPICS access occurs at this level."""
 
-    def __init__(self, schema):
-        self.screen = lcls_tools.common.devices.reader.create_screen(
-            schema.camera_area(_cfg.dev.beam_path, _cfg.dev.camera_name)
-        ).screens[_cfg.dev.camera_name]
+    def __init__(self):
+        self.device = slicops.device.Device(_cfg.dev.camera_name)
 
     def get_image(self):
         """Gets raw pixels from EPICS"""
-        try:
-            # TODO(pjm): the row/columns is reversed in lcls_tools
-            #  possibly this needs to be a flag in the camera meta data
-            return self.screen.image.reshape(self.screen.n_rows, self.screen.n_columns)
-        except PVInvalidError as err:
-            # TODO(pjm): could provide a better error message here
-            raise err
-        except AttributeError as err:
-            # most likely EPICS PV is unavailable due to timeout,
-            #  Screen.image should raise an exception if no connection is completed
-            #  for now, catch AttributeError: 'NoneType' object has no attribute 'reshape'
-            # TODO(pjm): could provide a better error message here
-            raise err
-        except ValueError as err:
-            # similar to the above, connection works but image IOC is misconfigured
-            # ex. cannot reshape array of size 0 into shape (1024,1024)
-            raise err
-
-    def get_pv(self, name):
-        """Get a PV value by name"""
-        pv = PV(self._pv_name(name))
-        v = pv.get()
-        if not pv.connected:
-            raise PVInvalidError(f"Unable to connect to PV: {n}")
-        return v
+        if (rv := self.device.get("image")) is None:
+            return ValueError("no image")
+        if not (
+            (r := self.device.get("num_rows")) and (c := self.device.get("num_cols"))
+        ):
+            raise ValueError("num_rows or num_cols is invalid")
+        return rv.reshape(c, r)
 
     def is_acquiring_images(self):
-        """Returns True if the camera's EPICS value indicates it is capturing images."""
         try:
-            return bool(self.get_pv("Acquire"))
-        except PVInvalidError as err:
+            return self.device.get("acquire")
+        except slicops.device.DeviceError as err:
             # does not return an error, the initial camera may not be currently available
             return False
 
     def start(self):
-        """Set the EPICS camera to acquire mode."""
-        self.put_pv("Acquire", 1)
+        self._set_acquire(1)
 
     def stop(self):
-        """Set the EPICS camera to stop acquire mode."""
-        self.put_pv("Acquire", 0)
+        self._set_acquire(0)
 
-    def put_pv(self, name, value):
-        """Set a PV value."""
-        pv = PV(self._pv_name(name))
-        v = pv.put(value)
-        if not pv.connected:
-            raise PVInvalidError(f"Unable to connect to PV: {n}")
-        if not v:
-            raise PVInvalidError(f"Update failed for PV: {n}")
-
-    def _pv_name(self, name):
-        return f"{self.screen.controls_information.control_name}:{name}"
+    def _set_acquire(self, is_on):
+        self.device.put("acquire", is_on)
 
 
 _cfg = pkconfig.init(
