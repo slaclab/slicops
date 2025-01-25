70a484511a84b0caf64159a7eadaa06fceff1da1
Paul Moeller <moeller@radiasoft.net>
2025-01-24 18:16:46 -0700

    Move state management to server (#21)

---
+++ slicops/ui_api/__init__.py
@@ -0,0 +1,13 @@
+"""UI APIs
+
+:copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdc, pkdlog, pkdp
+import slicops.ui_api.screen
+
+
+def api_classes():
+    return (slicops.ui_api.screen.API,)
