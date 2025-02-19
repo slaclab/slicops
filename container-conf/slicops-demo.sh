0ef276ce1ca51c87b3905182666608ec27c06b21
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-02-19 20:51:27 +0000

    Fix #67 set SLICOPS_CONFIG_UI_API_TCP_IP=0.0.0.0

--- container-conf/slicops-demo.sh
+++ container-conf/slicops-demo.sh
@@ -23,4 +23,4 @@ sleep 2
 Point your browser to http://localhost:$p
 To stop the server, type control-C here.
 EOF
-slicops service ui_api --prod --tcp-port="$p"
+SLICOPS_CONFIG_UI_API_TCP_IP=0.0.0.0 slicops service ui_api --prod --tcp-port="$p"
