683fdc3cf7eb2ba4ec4f80740df2c99ec20427b4
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-02-26 13:40:18 +0000

    verify int

--- slicops/device.py
+++ slicops/device.py
@@ -171,6 +171,8 @@ class _Accessor:
             raise AccessorPutError(f"read-only {self}")
         if self.meta.py_type == "bool":
             v = bool(value)
+        elif self.meta.py_type == "int":
+            v = int(value)
         else:
             raise AccessorPutError(f"unhandled py_type={self.meta.py_type} {self}")
         # ECA_NORMAL == 0 and None is normal, too, apparently
