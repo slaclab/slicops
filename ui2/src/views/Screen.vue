cae144a5c82d2f1da6afee0fab81b2a0521d4157
Paul Moeller <moeller@radiasoft.net>
2025-03-11 22:38:30 +0000

    for #76 generalized VApp for applications

--- ui2/src/views/Screen.vue
+++ ui2/src/views/Screen.vue
@@ -5,80 +5,9 @@
  http://github.com/slaclab/slicops/LICENSE
 -->
 <template>
-    <form>
-        <div
-            v-if="ui_ctx"
-            class="row">
-            <div
-                v-if="errorMessage"
-                class="alert alert-warning">{{ errorMessage }}
-            </div>
-            <VLayout
-                :layout="layout.value"
-                :ui_ctx="ui_ctx.value"
-            />
-        </div>
-    </form>
+    <VApp prefix="screen" />
 </template>
 
 <script setup>
- import { ref, reactive } from 'vue';
- import { apiService } from '@/service/api';
- import VLayout from '@/components/VLayout.vue';
-
- const errorMessage = ref('');
- const layout = reactive({});
- const ui_ctx = reactive({});
-
- const handleError = (error) => {
-     errorMessage.value = error;
- };
-
- const serverAction = (field, value) => {
-     errorMessage.value = '';
-     ui_ctx.value[field].enabled = false;
-     apiService.call(
-         `screen_${field}`, {
-             field_value: value,
-         },
-         (result) => {
-             updateUIState(result);
-             if (result.ui_ctx && result.ui_ctx[field] && 'enabled' in result.ui_ctx[field]) {
-             }
-             else {
-                 ui_ctx.value[field].enabled = true;
-             }
-         },
-         (err) => {
-             handleError(err);
-             ui_ctx.value[field].enabled = true;
-         }
-     );
- };
-
- const updateUIState = (result) => {
-     if (result.plot) {
-         ui_ctx.value.image = () => result.plot;
-     }
-     if (result.ui_ctx) {
-         Object.assign(ui_ctx.value, result.ui_ctx);
-     }
- }
-
- (() => {
-     apiService.call(
-         'screen_ui_ctx',
-         {},
-         (result) => {
-             ui_ctx.value = result.ui_ctx;
-             layout.value = result.layout;
-             ui_ctx.value.serverAction = serverAction;
-         },
-         (error) => {
-             camera.value = 'Error getting camera';
-         },
-     );
-
-     apiService.subscribe('screen_update', {}, updateUIState, handleError);
- })();
+ import VApp from '@/components/VApp.vue';
 </script>
