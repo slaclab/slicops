2d8bd7706ccb83643db8bd96c33192f2d8f15317
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-06-12 00:33:18 +0000

    ckp

--- slicops/device_db_sql.py
+++ slicops/device_db_sql.py
@@ -22,7 +22,7 @@ def _init():
     n = s + " index"
     # Since created once, no need for created/modified entries
     _meta = sql_db.Meta(
-        uri=pykern.sql_db.sqlite_uri(pykern.pkresource.file_path(_BASE_PATH)),
+        uri=pykern.sql_db.sqlite_uri(_path())
         schema=PKDict(
             beam_area=PKDict(
                 beam_area_id=p + " 1",
@@ -58,5 +58,131 @@ def _init():
                 pv_writable="bool",
                 py_type=s,
             ),
+            device_attr=PKDict(
+                device_attr_id=p + " 6",
+                device_attr_name=n,
+                device_attr_value=s,
+            ),
         ),
     )
+
+def insert(device_decl, session):
+    tables = insert.tables(as_dict=True)
+
+    def _area(name):
+        session.insert()
+    _area_id(device_decl.area)
+                        "area": "UNDS",
+                        "beam_path": (
+                            "CU_SXR",
+                            "SC_S2_X",
+                            "SC_SFTS",
+                            "SC_STMO",
+                            "SC_STXI",
+                            "SC_SXR",
+                        ),
+                        "device_kind": "screen",
+
+                        "area": "UNDS",
+                        "beam_path": (
+                            "CU_SXR",
+                            "SC_S2_X",
+                            "SC_SFTS",
+                            "SC_STMO",
+                            "SC_STXI",
+                            "SC_SXR",
+                        ),
+                        "device_kind": "screen",
+                        "device_length": 3661.222,
+                        "device_name": "BOD10",
+                        "pv_prefix": "YAGS:UNDS:3575",
+    for a in device_decl.accessor.values():
+        _areas(a)
+
+                        "accessor": PKDict(
+                            {
+                                "acquire": PKDict(
+                                    {
+                                        "name": "acquire",
+                                        "pv_base": "Acquire",
+                                        "pv_name": "YAGS:UNDS:3575:Acquire",
+                                        "pv_writable": True,
+                                        "py_type": "bool",
+                                    }
+                                ),
+                                "bit_depth": PKDict(
+                                    {
+                                        "name": "bit_depth",
+                                        "pv_base": "N_OF_BITS",
+                                        "pv_name": "YAGS:UNDS:3575:N_OF_BITS",
+                                        "py_type": "int",
+                                    }
+                                ),
+                                "image": PKDict(
+                                    {
+                                        "array_is_row_major": True,
+                                        "name": "image",
+                                        "pv_base": "Image:ArrayData",
+                                        "pv_name": "YAGS:UNDS:3575:Image:ArrayData",
+                                        "py_type": "ndarray",
+                                    }
+                                ),
+                                "num_cols": PKDict(
+                                    {
+                                        "name": "num_cols",
+                                        "pv_base": "Image:ArraySize1_RBV",
+                                        "pv_name": "YAGS:UNDS:3575:Image:ArraySize1_RBV",
+                                        "py_type": "int",
+                                    }
+                                ),
+                                "num_rows": PKDict(
+                                    {
+                                        "name": "num_rows",
+                                        "pv_base": "Image:ArraySize0_RBV",
+                                        "pv_name": "YAGS:UNDS:3575:Image:ArraySize0_RBV",
+                                        "py_type": "int",
+                                    }
+                                ),
+                            }
+                        ),
+                        "area": "UNDS",
+                        "beam_path": (
+                            "CU_SXR",
+                            "SC_S2_X",
+                            "SC_SFTS",
+                            "SC_STMO",
+                            "SC_STXI",
+                            "SC_SXR",
+                        ),
+                        "device_kind": "screen",
+                        "device_length": 3661.222,
+                        "device_name": "BOD10",
+                        "pv_prefix": "YAGS:UNDS:3575",
+                    }
+
+    pass
+
+
+def recreate(devices):
+    assert not _meta
+    pykern.pkio.unchecked_remove(_path)
+    _init()
+    return _Inserter(devices, _meta).counts
+
+
+def _path():
+    return pykern.pkresource.file_path(_BASE_PATH)
+
+
+def _Inserter:
+    def __init__(self, devices, meta):
+        self.counts = PKDict()
+        with meta.session() as s:
+            self.session = s
+            self.tables = meta.tables(as_dict=True)
+            self.beam_areas = PKDict(self._beam_areas(devices, tables.beam_area))
+        self.counts =
+
+    def _beam_areas(self, devices, table):
+        for n in sorted(set(d.area for d in devices)):
+            yield n, s.insert(table, beam_area_name=n).beam_area_id
--- slicops/pkcli/device_db.py
+++ slicops/pkcli/device_db.py
@@ -9,41 +9,47 @@ TODO(robnagler): document, correct types, add machine and area_to_machine, beam_
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdlog, pkdp
 import importlib
-import pykern.device_db
+
+# import pykern.device_db
 import pykern.pkio
 import pykern.pkyaml
 import re
 
+from pykern import pkjson
+
+
 # We assume the names are valid (could check, but no realy point)
 # What this test is doing is ensuring we understand the structure of a pv_base
-_PV_BASE_RE = r"(\w{1,60}|\w{1,58}:\w{1,58})"
+_PV_POSTFIX_RE = r"([\w.]{1,60}|\w{1,58}:[\w.]{1,58})"
 
 # Eventually would be canonical
 _DEVICE_KIND_TO_ACCESSOR = PKDict(
     screen=PKDict(
         {
+            # Everything here should have py_type
             "Acquire": PKDict(name="acquire", py_type="bool", pv_writable=True),
-            "Gain": PKDict(name="gain", pv_writable=True),
+            "Gain": PKDict(name="gain", pv_writable=True, py_type="int"),
             "IMAGE": PKDict(name="image", py_type="ndarray"),
             "Image:ArrayData": PKDict(name="image", py_type="ndarray"),
-            "Image:ArraySize0_RBV": PKDict(name="num_rows"),
-            "Image:ArraySize1_RBV": PKDict(name="num_cols"),
-            "Image:ArraySizeX_RBV": PKDict(name="num_cols"),
-            "Image:ArraySizeY_RBV": PKDict(name="num_rows"),
-            "N_OF_BITS": PKDict(name="bit_depth"),
-            "N_OF_COL": PKDict(name="num_cols"),
-            "N_OF_ROW": PKDict(name="num_rows"),
+            "Image:ArraySize0_RBV": PKDict(name="num_rows", py_type="int"),
+            "Image:ArraySize1_RBV": PKDict(name="num_cols", py_type="int"),
+            "Image:ArraySizeX_RBV": PKDict(name="num_cols", py_type="int"),
+            "Image:ArraySizeY_RBV": PKDict(name="num_rows", py_type="int"),
+            "N_OF_BITS": PKDict(name="bit_depth", py_type="int"),
+            "N_OF_COL": PKDict(name="num_cols", py_type="int"),
+            "N_OF_ROW": PKDict(name="num_rows", py_type="int"),
             # Devlement: AreaDetector SimDetector default definitions
             # TODO(robnagler) simplify the st.cmd for the SimDetector
-            "cam1:SizeX": PKDict(name="num_cols"),
-            "cam1:SizeY": PKDict(name="num_rows"),
-            "cam1:N_OF_BITS": PKDict(name="bit_depth"),
+            "cam1:SizeX": PKDict(name="num_cols", py_type="int"),
+            "cam1:SizeY": PKDict(name="num_rows", py_type="int"),
+            "cam1:N_OF_BITS": PKDict(name="bit_depth", py_type="int"),
             "cam1:Acquire": PKDict(name="acquire", py_type="bool", pv_writable=True),
-            "cam1:Gain": PKDict(name="gain", pv_writable=True),
+            "cam1:Gain": PKDict(name="gain", pv_writable=True, py_type="int"),
             "image1:ArrayData": PKDict(name="image", py_type="ndarray"),
         }
     )
 )
+
 
 # TODO(robnagler) should be dynamic, but need to add to paths so easiest to add here for now
 _DEV_YAML = """
@@ -64,83 +70,79 @@ screens:
       - DEV_BEAM_PATH
       sum_l_meters: 0.614
       type: PROF
-      array_is_row_major: true
 """
 
 
-def to_python():
-    """Convert device yaml file to python file
+def create_from_yaml():
+    """Convert device yaml file to db"""
+    pass  # return slicops.device_db_sql.recreate(tuple(_Parser().devices.values()))
 
-    Returns:
-      pykern.pkio.py_path: Path of python file
-    """
-    return _Parser().output_path
+
+def parse():
+    r = pykern.pkio.mkdir_parent("raw")
+    x = set()
+    for d in _Parser().devices.values():
+        x.update(d.attrs.keys())
+        pykern.pkjson.dump_pretty(
+            d,
+            filename=pykern.pkio.mkdir_parent(r.join(d.device_kind, d.area)).join(
+                d.device_name + ".json"
+            ),
+        )
+    return x
 
 
 class _Parser:
     def __init__(self):
-        self._map = PKDict(
-            AREA_TO_BEAM_PATH=PKDict(),
-            AREA_TO_DEVICE=PKDict(),
-            BEAM_PATH_TO_AREA=PKDict(),
-            BEAM_PATH_TO_DEVICE=PKDict(),
-            DEVICE_KIND_TO_DEVICE=PKDict(),
-            DEVICE_TO_AREA=PKDict(),
-            DEVICE_TO_META=PKDict(),
-        )
-        self._init_paths()
+        self._init()
         self._parse()
 
-    def _init_paths(self):
+    def _init(self):
         self._yaml_glob = (
             pykern.pkio.py_path(
-                importlib.import_module("lcls_tools.common.devices.yaml").__file__
+                pkdp(importlib.import_module("lcls_tools.common.devices.yaml").__file__)
             )
             .dirpath()
             .join("*.yaml")
         )
+        self.devices = PKDict()
 
     def _parse(self):
         for p in pykern.pkio.sorted_glob(self._yaml_glob):
+            if p.basename == "beampaths.yaml":
+                continue
             try:
-                self._parse_file(pykern.pkyaml.load_file(p))
+                self._parse_file(pykern.pkyaml.load_file(p), p)
             except Exception:
                 pkdlog("ERROR file={}", p)
                 raise
         if pykern.pkconfig.in_dev_mode():
-            self._parse_file(pykern.pkyaml.load_str(_DEV_YAML))
+            self._parse_file(
+                pykern.pkyaml.load_str(_DEV_YAML), pykern.pkio.py_path(".")
+            )
 
-    def _parse_file(self, src):
+    def _parse_file(self, src, path):
 
         def _accessor(meta):
-            a = _DEVICE_KIND_TO_ACCESSOR[meta.device_kind]
-            rv = PKDict(
-                {
-                    a[k]
-                    .name: a[k]
-                    .copy()
-                    .pksetdefault(pv_base=k, pv_name=v, py_type="int")
-                    for k, v in meta.pv_base.items()
-                    if k in a
-                }
-            )
-            if "image" in rv:
-                rv.image.array_is_row_major = meta.array_is_row_major
-            return rv
+            a = _DEVICE_KIND_TO_ACCESSOR.screen  # [meta.device_kind]
+            for k, v in meta.pv_base.items():
+                rv = a[k].copy() if k in a else PKDict()
+                yield k, rv.pksetdefault(pv_base=k, pv_name=v)
 
         def _assign(device, meta):
             """Corrections to input data"""
-            if d := self._map.DEVICE_TO_META.get(n):
+            if d := self.devices.get(n):
                 raise ValueError(f"duplicate device={n} record={r} first meta={d}")
-            self._map.DEVICE_TO_META[n] = meta
+            self.devices[n] = meta
 
         def _input_fixups(device, rec):
-            """Corrections to input data"""
+            """NOT USED Corrections to input data"""
             # TODO(robnagler) when there is resolution
-            # if device == "VCCB":
-            #    rec.controls_information.PVs.resolution = "CAMR:LGUN:950:RESOLUTION"
+            if device == "VCCB":
+                rec.controls_information.PVs.resolution = "CAMR:LGUN:950:RESOLUTION"
+            return
             # Don't need resolution
-            rec.controls_information.PVs.pkdel("resolution")
+            # rec.controls_information.PVs.pkdel("resolution")
             if rec.metadata.sum_l_meters is None:
                 # eg. LI25.yaml OTR22
                 return False
@@ -149,51 +151,56 @@ class _Parser:
                 return False
             return True
 
-        def _meta(device, ctl, md, kind):
+        def _meta(device, ctl, md, kind, raw):
             rv = PKDict(pv_prefix=ctl.control_name, device_kind=kind, pv_base=PKDict())
-            p = re.compile(rf"^{re.escape(rv.pv_prefix)}:{_PV_BASE_RE}$")
+            p = re.compile(rf"^{re.escape(rv.pv_prefix)}:{_PV_POSTFIX_RE}$")
+            # TODO(robnagler) type and kind are different, or kind
             for v in ctl.PVs.values():
                 if not (m := p.search(v)):
                     raise ValueError(
                         f"invalid pv name={v} for device={device} regex={p}"
                     )
                 rv.pv_base[m.group(1)] = v
-            return rv.pkupdate(
-                array_is_row_major=md.get("array_is_row_major", True),
-                # TODO(robnagler) meta.type is not always set (see vcc.yaml), so ignoring for now
+            rv.pkupdate(
                 area=md.area,
                 beam_path=tuple(sorted(md.beam_path)),
-                device_length=md.sum_l_meters,
                 device_name=device,
+                # Not always a type(?)
+                device_type=md.get("type"),
             )
+            md.pkdel("beam_path")
+            md.pkdel("area")
+            md.pkdel("type")
+            # TODO robnagler list of attrs {'bpms_before_wire', 'bpms_after_wire', 'l_eff', 'lblms', 'rf_freq', 'sum_l_meters'}
+            rv.attrs = md
+            return rv
 
         def _meta_fixups(meta):
             # TODO(robnagler): DEV_CAMERA is special, need to configure it right
-            if meta.device_name != "DEV_CAMERA":
-                meta.pv_base.pksetdefault(
-                    Acquire=f"{meta.pv_prefix}:Acquire",
-                )
-            meta.accessor = _accessor(meta)
+            # if meta.device_name != "DEV_CAMERA":
+            #    meta.pv_base.pksetdefault(
+            #        Acquire=f"{meta.pv_prefix}:Acquire",
+            #    )
+            meta.accessor = PKDict(_accessor(meta))
             # No longer needed
             meta.pkdel("pv_base")
-            meta.pkdel("array_is_row_major")
             return meta
 
-        if not (s := src.get("screens")):
-            return
-        for n, r in s.items():
-            try:
-                if not _input_fixups(n, r):
-                    continue
-                _assign(
-                    n,
-                    _meta_fixups(
-                        _meta(n, r.controls_information, r.metadata, "screen")
-                    ),
-                )
-            except Exception:
-                pkdlog("ERROR device={} record={}", n, r)
-                raise
-
-    def _to_db(self):
-        self._map
+        for k, x in src.items():
+            for n, r in x.items():
+                try:
+                    _input_fixups(n, r)
+                    # if not _input_fixups(n, r):
+                    #    continue
+                    _assign(
+                        n,
+                        _meta_fixups(
+                            # remove the s as all the kinds are plural
+                            # TODO(robnagler) look up these in table
+                            #   reconcile with type
+                            _meta(n, r.controls_information, r.metadata, k[:-1], r)
+                        ),
+                    )
+                except Exception:
+                    pkdlog("ERROR device={} record={}", n, r)
+                    raise
