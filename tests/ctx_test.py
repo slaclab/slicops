57715b31e1231db7cb373e81c677b10c93048658
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-07-05 01:59:24 +0000

    ctx_test checkign

---
+++ tests/ctx_test.py
@@ -0,0 +1,20 @@
+"""test ctx
+
+:copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+
+
+def test_cases():
+    from pykern import pkdebug, pkjson, pkunit
+    from pykern.pkcollections import PKDict
+    from slicops import ctx
+
+    for d in pkunit.case_dirs():
+        try:
+            r = ctx.Ctx("input", path=d)
+            r = PKDict(fields=r._fields, ui_layout=r._ui_layout.rows)
+        except Exception as e:
+            pkdebug.pkdlog('{}', pkdebug.pkdexc())
+            r = PKDict(error=e)
+        pkjson.dump_pretty(r, filename=d.join(f"out.json"))
