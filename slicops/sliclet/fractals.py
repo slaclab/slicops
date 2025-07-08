12c6705bcfc172bd6cc9aea24976efe78e06c27e
Paul Moeller <moeller@radiasoft.net>
2025-07-08 22:36:09 +0000

    removed old fractal code

--- slicops/sliclet/fractals.py
+++ slicops/sliclet/fractals.py
@@ -7,68 +7,16 @@
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 import numpy as np
-import slicops.plot
 import slicops.sliclet.simple
 
 
 class Fractals(slicops.sliclet.simple.Simple):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def handle_ctx_set_mode(self, txn, value, **kwargs):
         j = value == "Julia"
         txn.multi_set(
             ("density_i.ui.visible", j),
             ("density_r.ui.visible", j),
-        )
-
-    def x_handle_ctx_set_save(self, txn, **kwargs):
-        super().handle_ctx_set_save(txn, **kwargs)
-        self.__update_plot(txn)
-
-    def __generate_mandelbrot(
-        self, width, height, x_min, x_max, y_min, y_max, max_iterations
-    ):
-        # Create a grid of complex numbers
-        x = np.linspace(x_min, x_max, width)
-        y = np.linspace(y_min, y_max, height)
-        C = x + 1j * y[:, np.newaxis]
-        Z = np.zeros_like(C, dtype=np.complex128)
-        M = np.full(C.shape, max_iterations, dtype=int)
-        for i in range(max_iterations):
-            # Find points that haven't escaped
-            mask = np.abs(Z) <= 2.0
-            # Update Z for non-escaped points
-            Z[mask] = Z[mask] * Z[mask] + C[mask]
-
-            # Update escape times for newly escaped points
-            escaped_mask = (np.abs(Z) > 2.0) & mask
-            M[escaped_mask] = i
-        return M
-
-    def __update_plot(self, txn):
-        if not txn.ui_get("plot", "visible"):
-            txn.multi_set((("plot.ui.visible", True), ("color_map.ui.visible", True)))
-
-        # Image parameters
-        width, height = (800,) * 2
-        x_min, x_max = -2.0, 1.0
-        y_min, y_max = -1.5, 1.5
-        max_iterations = 3
-        # Generate the fractal
-        image = pkdp(
-            self.__generate_mandelbrot(
-                width, height, x_min, x_max, y_min, y_max, max_iterations
-            )
-        )
-        txn.field_set(
-            "plot",
-            pkdp(
-                PKDict(
-                    raw_pixels=image,
-                )
-            ),
         )
 
 
