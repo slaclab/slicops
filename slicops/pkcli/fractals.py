6166c731ee608c5f059d83565ae20caf10614482
Paul Moeller <moeller@radiasoft.net>
2025-07-09 15:50:41 +0000

    run fmt

--- slicops/pkcli/fractals.py
+++ slicops/pkcli/fractals.py
@@ -3,6 +3,7 @@
 :copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
 :license: http://github.com/slaclab/slicops/LICENSE
 """
+
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdlog, pkdp
 import datetime
@@ -14,38 +15,47 @@ import time
 
 _SLICLET = "fractals"
 
+
 def forever(period):
     pkdlog("database={}", path())
     if (period := int(period)) < 3:
         pykern.pkcli.command_error("period too short={}", period)
     while True:
-        #TODO(robnagler) exceptions
+        # TODO(robnagler) exceptions
         pkdlog("computing")
         once()
         pkdlog("sleeping")
         time.sleep(period)
 
+
 def once():
     p = slicops.pkcli.simple.read(_SLICLET)
     g = _compute(p)
-    rv = PKDict(plot_file = str(path()))
+    rv = PKDict(plot_file=str(path()))
     if g is None:
         pykern.pkio.unchecked_remove(rv.plot_file)
         rv.message = f"invalid method={p.method}"
     else:
         numpy.save(str(rv.plot_file), g)
-        rv.message = "Finished: " + datetime.datetime.utcnow().isoformat(timespec='seconds')
+        rv.message = "Finished: " + datetime.datetime.utcnow().isoformat(
+            timespec="seconds"
+        )
     slicops.pkcli.simple.write(_SLICLET, rv)
+
 
 def path():
     return slicops.pkcli.simple.path(_SLICLET).new(basename="fractals.npy")
 
+
 def _compute(params):
     if params.mode == "Julia":
-        return _julia(complex(params.density_r, params.density_i), params.size, params.iterations)
+        return _julia(
+            complex(params.density_r, params.density_i), params.size, params.iterations
+        )
     if params.mode == "Mandelbrot":
         return _mandelbrot(params.size, params.iterations)
     return None
+
 
 def _julia(c, num_iterations, size):
     x_min, x_max = (-1.5, 1.5)
@@ -63,7 +73,7 @@ def _julia(c, num_iterations, size):
 
     for i in range(num_iterations):
         # Apply the Julia set formula only to non-diverged points
-        Z[~diverged] = Z[~diverged]**2 + c + random_perturbation
+        Z[~diverged] = Z[~diverged] ** 2 + c + random_perturbation
 
         # Check for divergence (magnitude > 2)
         current_diverged = (numpy.abs(Z) > 2) & (~diverged)
@@ -77,6 +87,7 @@ def _julia(c, num_iterations, size):
     # Assign num_iterations to points that didn't diverge
     iterations[~diverged] = num_iterations
     return iterations
+
 
 def _mandelbrot(size, iterations):
     x_min, x_max = (-2.0, 1.0)
@@ -95,7 +106,11 @@ def _mandelbrot(size, iterations):
         mask = numpy.abs(Z) <= 2.0
 
         # Update Z for non-escaped points
-        Z[mask] = (1 + random_perturbation) * Z[mask] * Z[mask] + C[mask] + random_perturbation
+        Z[mask] = (
+            (1 + random_perturbation) * Z[mask] * Z[mask]
+            + C[mask]
+            + random_perturbation
+        )
 
         # Update escape times for newly escaped points
         escaped_mask = (numpy.abs(Z) > 2.0) & mask
