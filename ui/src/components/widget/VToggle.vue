0e5f43071ee3d9fb1bb1b38331cc050b9b169913
Paul Moeller <moeller@radiasoft.net>
2025-07-10 23:19:56 +0000

    for #123 work-around revert problem, and unmount correctly

--- ui/src/components/widget/VToggle.vue
+++ ui/src/components/widget/VToggle.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script setup>
- import { ref, onMounted } from 'vue';
+ import { onBeforeUnmount, onMounted, ref, watch } from 'vue';
  import VLabel from '@/components/widget/VLabel.vue';
 
  const props = defineProps({
@@ -32,11 +32,21 @@
      props.ctx.serverAction(props.field, props.ctx[props.field].value);
  };
 
+ onBeforeUnmount(() => {
+     toggleElement.value.bootstrapToggle('destroy');
+ });
+
  onMounted(() => {
      const l = props.ctx[props.field].ui.toggle_labels;
      toggleElement.value.bootstrapToggle({
          offlabel: l[0],
          onlabel: l[1],
      });
+ });
+
+ watch(() => props.ctx[props.field].value, () => {
+     // work-around for missing render on revert changes
+     toggleElement.value.bootstrapToggle(
+         props.ctx[props.field].value ? 'on' : 'off');
  });
 </script>
