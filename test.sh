5c6d25032f7986e12746b6127280344c2658e922
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-08-08 21:58:29 +0000

    mock caRepeater

--- test.sh
+++ test.sh
@@ -4,12 +4,23 @@
 #
 set -euo pipefail
 
+_carepeater() {
+    if [[ $(type -p caRepeater) ]]; then
+        return
+    fi
+    declare d=
+    echo 'sleep 10000' > run/caRepeater
+    chmod +x run/caRepeater
+    export PATH=$(cd "$(dirname "$0")"; pwd):$PATH
+}
+
 _err() {
     _msg "$@"
     return 1
 }
 
 _main() {
+    _carepeater
     export EPICS_CA_ADDR_LIST=127.0.0.1
     export EPICS_CA_AUTO_ADDR_LIST=no
     export PYKERN_PKCLI_TEST_MAX_PROCS=4
