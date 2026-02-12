e05a50ae550a883f85fd6a77279ec5fb61a20b27
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-02-12 22:45:49 +0000

    Fix #190 upgrade epics and nvm

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -13,15 +13,12 @@ build_vars() {
 }
 
 build_as_root() {
-    install_yum_install \
-        libXt-devel \
-        motif-devel \
-        nodejs \
-        perl-FindBin
+    install_yum_install libatomic
 }
 
 build_as_run_user() {
     _slicops_bashrc
+    _slicops_nvm_install
     install_repo_eval epics-area-detector
     _slicops_pkg_install
     _slicops_bin
@@ -34,25 +31,28 @@ export EPICS_BASE=$HOME/.local/epics
 # $EPICS_BASE/startup/EpicsHostArch outputs linux-x86_64; no need to be dynamic here
 export EPICS_HOST_ARCH=linux-x86_64
 bivio_path_insert "$EPICS_BASE/bin/$EPICS_HOST_ARCH"
-
-# for newer nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 EOF
     install_source_bashrc
 }
 
 _slicops_bin() {
     build_replace_vars "$build_guest_conf"/slicops-entry.sh "$_slicops_entry"
-    # POSIT: epics-install.sh sets sim_det_dir
+    sim_det_dir=$(find "$EPICS_BASE" -type d -path '*iocBoot/iocSimDetector')
     build_replace_vars "$build_guest_conf"/slicops-demo.sh "$_slicops_demo"
-    # Sanity check that the file contains somethng
+    # Sanity check that the file contains something
     if ! grep sim-detector "$_slicops_demo" &> /dev/null; then
         cat "$_slicops_demo"
         install_err "$_slicops_demo was not generated properly"
     fi
     chmod a+rx "$_slicops_demo" "$_slicops_entry"
+}
+
+_slicops_nvm_install() {
+    # POSIT: same as download/**/codes/common.sh
+    mkdir -p "$NVM_DIR"
+    PROFILE=/dev/null | curl -s -S -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    install_source_bashrc
+    nvm install 25.6.1
 }
 
 _slicops_pip_install() {
@@ -71,11 +71,6 @@ _slicops_pkg_install() {
     _slicops_pip_install radiasoft/pykern "$PYKERN_BRANCH"
     _slicops_pip_install slaclab/slicops "$SLICOPS_BRANCH"
     cd slaclab/slicops/ui
-    rm ~/.npmrc
-    # upgrade nvm and node
-    curl -s -S -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-    source $HOME/.nvm/nvm.sh
-    nvm install node
     npm install
     npm run build
     mv dist ../slicops/package_data/vue
