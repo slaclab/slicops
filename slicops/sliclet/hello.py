8260089ae67bd8211e24a47a8187de46b7d0d1ae
Paul Moeller <moeller@radiasoft.net>
2026-03-05 21:35:59 +0000

    fix #196 hello world sliclet

---
+++ slicops/sliclet/hello.py
@@ -0,0 +1,12 @@
+"""Hello World Sliclet
+
+:copyright: Copyright (c) 2026 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+import slicops.sliclet
+
+class Hello(slicops.sliclet.Base):
+    pass
+
+CLASS = Hello
