6a708a3751928cef296f538a9c372e19cfda38c9
Christopher Zimmer <zimmerc@dev-srv09.slac.stanford.edu>
2026-03-16 15:36:14 -0700

    intro slide 9: added a buton

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -5,10 +5,16 @@
 """
 
 import slicops.sliclet
+from pykern.pkcollections import PKDict
 
+_MESSAGE = PKDict(Bye="Ta Ta!", Hello="Hello World!")
+_LABEL_FLIP = PKDict(Bye="Hello", Hello="Bye")
 
 class Hello(slicops.sliclet.Base):
-    pass
+    def on_click_greeting(self, txn, **kwargs):
+        x = txn.group_attr("greeting.ui.label")
+        txn.field_value_set("message", _MESSAGE[x])
+        txn.group_attr_set("greeting.ui.label", _LABEL_FLIP[x])
 
 
 CLASS = Hello
