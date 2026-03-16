5e48964a9b7fd5b0e45acf63db8e9acc8ea56b96
Tyler Kabana <kabanaty@dev-srv09.slac.stanford.edu>
2026-03-16 15:34:41 -0700

    Updated hello files

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -5,10 +5,19 @@
 """
 
 import slicops.sliclet
+from pykern.pkcollections import PKDict
 
+
+_MESSAGE = PKDict(Bye = "Ta Ta!", Hello = "Hello World!")
+_LABEL_FLIP = PKDict(Bye = "Hello", Hello = "Bye")
 
 class Hello(slicops.sliclet.Base):
-    pass
+    def on_click_bye(self, txn, **kwargs):
+        txn.field_value_set("message", "Ta Ta!")
 
+    def on_click_greeting(self, txn, **kwargs):
+        x = txn.group_attr("greeting.ui.label")
+        txn.field_value_set("message", _MESSAGE[x])
+        txn.group_attr_set("greeting.ui.label", _LABEL_FLIP[x])
 
 CLASS = Hello
