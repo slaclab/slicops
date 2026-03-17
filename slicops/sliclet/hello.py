62e8033b60f823fe5eadd1a97fee0088592b17d4
Nicole Neveu <nneveu@slac.stanford.edu>
2026-03-17 13:42:56 -0700

    display acquire status

---
+++ slicops/sliclet/hello.py
@@ -0,0 +1,35 @@
+"""Hello World Sliclet
+
+:copyright: Copyright (c) 2026 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
+import slicops.sliclet
+import slicops.device
+
+
+class Hello(slicops.sliclet.Base):
+
+    def handle_destroy(self):
+        if self.__device:
+            self.__device.destroy()
+
+    def handle_init(self, txn):
+        self.__device = None
+
+    def handle_start(self, txn):
+        self.__device = slicops.device.Device("DEV_CAMERA")
+        # TODO
+
+    def __handle_acquire(self, change):
+        def _msg():
+            # TODO
+            return "Idle"
+
+        # TODO
+        pass
+
+
+CLASS = Hello
