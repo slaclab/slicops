7afc84c7396e6e52323aa786bd71667ae1c7c1fd
KuktaeKim <ktkim@slac.stanford.edu>
2026-03-17 14:21:15 -0700

    threading producer-consumer

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -4,15 +4,14 @@
 :license: http://github.com/slaclab/slicops/LICENSE
 """
 
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 import slicops.sliclet
 import slicops.device
-from pykern.pkcollections import PKDict
-
-_MESSAGE = PKDict(Bye="Ta Ta!", Hello="Hello World!")
-_LABEL_FLIP = PKDict(Bye="Hello", Hello="Bye")
 
 
 class Hello(slicops.sliclet.Base):
+
     def handle_destroy(self):
         if self.__device:
             self.__device.destroy()
@@ -20,24 +19,24 @@ class Hello(slicops.sliclet.Base):
     def handle_init(self, txn):
         self.__device = None
 
-    def on_click_greeting(self, txn, **kwargs):
-        def _n_col():
-            if self.__device is None:
-                self.__device = slicops.device.Device("DEV_CAMERA")
-            return self.__device.accessor("n_col").get()
+    def handle_start(self, txn):
+        self.__device = slicops.device.Device("DEV_CAMERA")
+        self.__device.accessor("acquire").monitor(self.__handle_acquire)
 
-            # if not hasattr(self, "__device"):
-            #     self.__device = slicops.device.Device("DEV_CAMERA")
-            # return self.__device.accessor("n_col").get()
+    def __handle_acquire(self, change):
+        def _msg():
+            if "connected" in change:
+                return None
+            if "error" in change:
+                return f"Error: {change.error}"
+            if change.value:
+                return "Acquiring"
+            return "Idle"
 
-        x=txn.group_attr("greeting", "ui", "label")
-        txn.field_value_set(
-            "message",
-            f"{_MESSAGE[x]} {_n_col()}",
-        )
-        txn.group_attr_set("greeting.ui.label", _LABEL_FLIP[x])
-
-
+        if (m := _msg()) is None:
+            return
+        with self.lock_for_update() as txn:
+            txn.field_value_set("status", m)
 
 
 CLASS = Hello
--- tests/sliclet/hello_test.py
+++ tests/sliclet/hello_test.py
@@ -6,22 +6,15 @@
 
 import pytest
 
-# asyncio/async/await is boilerplate
-@pytest.mark.asyncio(loop_scope="module")
-async def test_greeting():
-    from slicops import mock_epics
-    mock_epics.reset_state()
-    from slicops import unit_util
-    # starts tornado and vite on unique ports; shuts down
-    async with unit_util.SlicletSetup("hello") as s:
-        # provides lots of useful utilities, for now comparison
-        from pykern import pkunit
-        # get first ctx_update; vuew bootstraps: all fields and ui_layout
-        r = await s.ctx_update()
-        pkunit.pkeq("Hello World!", r.fields.message.value)
-        # click greeting button (value required and must be None)
-        await s.ctx_field_value_set(greeting=None)
-        # next update should contain changed message
-        r = await s.ctx_update()
-        pkunit.pkre(r"Ta Ta! \d+", r.fields.message.value)
 
+@pytest.mark.asyncio(loop_scope="module")
+async def test_status():
+    from slicops import unit_util
+
+    with unit_util.start_ioc("cam1"):
+        async with unit_util.SlicletSetup("hello") as s:
+            from pykern import pkunit
+
+            r = await s.ctx_update()
+            pkunit.pkeq("Initializing", r.fields.status.value)
+            # TODO
