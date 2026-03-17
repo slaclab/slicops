af692ce61fe5bd4be0e38fb02cc3047406fce6ca
Tyler Kabana <kabanaty@dev-srv09.slac.stanford.edu>
2026-03-17 15:06:10 -0700

    threading producer-consumer

--- tests/sliclet/hello_test.py
+++ tests/sliclet/hello_test.py
@@ -8,17 +8,17 @@ import pytest
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_greeting():
+async def test_status():
     from slicops import unit_util
 
-    async with unit_util.SlicletSetup("hello") as s:
-        from pykern import pkunit
+    with unit_util.start_ioc("cam1"):
+        async with unit_util.SlicletSetup("hello") as s:
+            from pykern import pkunit
 
-        r = await s.ctx_update()
-        pkunit.pkeq(r"Hello World!", r.fields.message.value)
-        
-        await s.ctx_field_value_set(greeting=None)
-        
-        r = await s.ctx_update()
-        pkunit.pkre(r"Ta Ta! \d+", r.fields.message.value)
-        pass
+            r = await s.ctx_update()
+            pkunit.pkeq("Initializing", r.fields.status.value)
+            r = await s.ctx_update()
+            pkunit.pkeq("Connected", r.fields.status.value)
+            r = await s.ctx_update()
+            pkunit.pkeq("Idle", r.fields.status.value)
+            
