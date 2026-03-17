de123a56766391d43eb1a18f6b2b529b22224e67
Shamin Chowdhury <shamin@slac.stanford.edu>
2026-03-17 14:14:10 -0700

    mock ioc template

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -19,17 +19,26 @@ class Hello(slicops.sliclet.Base):
     def handle_init(self, txn):
         self.__device = None
 
+    # called in thread so we can do "work" like create device
     def handle_start(self, txn):
         self.__device = slicops.device.Device("DEV_CAMERA")
-        # TODO
+        self.__device.accessor("acquire").monitor(
+            self.__handle_acquire
+        )
 
+    # monitor callback happens in an EPICS thread
     def __handle_acquire(self, change):
         def _msg():
-            # TODO
+            if "connected" in change:
+                return "Connected"
+            if "error" in change:
+                return f"Error: {change.error}"
+            if change.value:
+                return "Acquiring"
             return "Idle"
 
-        # TODO
-        pass
+        with self.lock_for_update() as txn:
+            txn.field_value_set("status", _msg())
 
 
 CLASS = Hello
