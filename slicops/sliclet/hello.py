3f46a51b1c68c7c30c2bf01d7d28e94c2aca4da8
Gopika Bhardwaj <gopikab@dev-srv09.slac.stanford.edu>
2026-03-17 09:56:34 -0700

    day 1 work

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -5,11 +5,36 @@
 """
 
 import slicops.sliclet
+from pykern.pkcollections import PKDict
+import slicops.device
 
+_MESSAGE = PKDict(Bye="Ta Ta!", Hello="Hello World!")
+_LABEL_FLIP = PKDict(Bye="Hello", Hello="Bye")
 
 class Hello(slicops.sliclet.Base):
-    def on_click_bye(self, txn, **kwargs):
-        pass
+    def handle_destroy(self):
+        if self.__device:
+            self.__device.destroy()
+
+    def handle_init(self, txn):
+        self.__device = None
+
+    def on_click_greeting(self, txn, **kwargs):
+        def _n_col():
+            if not hasattr(self, "__device"):
+                self.__device = slicops.device.Device(
+                        "DEV_CAMERA")
+            return self.__device.accessor(
+                    "n_col").get()
+        x = txn.group_attr("greeting", "ui", "label")
+        txn.field_value_set(
+                "message",
+                f"{_MESSAGE[x]} {_n_col()}",
+                )
+
+        #x = txn.group_attr("greeting.ui.label")
+        #txn.field_value_set("message", _MESSAGE[x])
+        txn.group_attr_set("greeting.ui.label", _LABEL_FLIP[x])
 
 
 CLASS = Hello
