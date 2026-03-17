7edb948b73429eff0bb3c39385019d1301b1e71d
Nicole Neveu <nneveu@slac.stanford.edu>
2026-03-17 09:54:52 -0700

    hello examples and tests

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -5,10 +5,31 @@
 """
 
 import slicops.sliclet
+from pykern.pkcollections import PKDict
+import slicops.device
 
+_MESSAGE = PKDict(Bye="Ta Ta!", Hello="Hello World!")
+_LABEL_FLIP = PKDict(Bye="Hello", Hello="Bye")
 
 class Hello(slicops.sliclet.Base):
-    pass
+    def handle_destroy(self):
+        if self.__device:
+            self.__device.destroy()
 
+    def handle_init(self, txn):
+        self.__device = None
+
+    def on_click_greeting(self, txn, **kwargs):
+        def _n_col():
+            if self.__device is None:
+                self.__device = slicops.device.Device("DEV_CAMERA")
+            return self.__device.accessor("n_col").get()
+
+        x = txn.group_attr("greeting", "ui", "label")
+        txn.field_value_set(
+            "message", 
+            f"{_MESSAGE[x]}{_n_col()}",
+         )
+        txn.group_attr_set("greeting.ui.label", _LABEL_FLIP[x])
 
 CLASS = Hello
---
+++ tests/sliclet/hello_test.py
@@ -0,0 +1,20 @@
+"""Test hello
+
+:copyright: Copyright (c) 2026 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+import pytest
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_greeting():
+    from slicops import unit_util
+
+    async with unit_util.SlicletSetup("hello") as s:
+        from pykern import pkunit
+        r = await s.ctx_update()
+        pkunit.pkeq("Hello World!", r.fields.message.value)
+        await s.ctx_field_value_set(greeting=None)
+        r = await s.ctx_update()
+        pkunit.pkeq("Ta Ta! \d+", r.fields.message.value)
