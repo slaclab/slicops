c93a319d31bf87ca743662aead1830006f07fa61
William Colocho <colocho@stanford.edu>
2026-03-17 14:14:03 -0700

    mock ioc template

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -9,7 +9,26 @@ from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 import slicops.sliclet
 import slicops.device
 
+"""
+def handle_start(self, txn):
+        self.__device = slicops.device.Device("DEV_CAMERA")
+        self.__device.accessor("acquire").monitor(self.__handle_acquire)
 
+    def __handle_acquire(self, change):
+        def _msg():
+            if "connected" in change:
+                return None
+            if "error" in change:
+                return f"Error: {change.error}"
+            if change.value:
+                return "Acquiring"
+            return "Idle"
+
+        if (m := _msg()) is None:
+            return
+        with self.lock_for_update() as txn:
+            txn.field_value_set("status", m)
+"""
 class Hello(slicops.sliclet.Base):
 
     def handle_destroy(self):
@@ -21,14 +40,26 @@ class Hello(slicops.sliclet.Base):
 
     def handle_start(self, txn):
         self.__device = slicops.device.Device("DEV_CAMERA")
+        self.__device.accessor("acquire").monitor(self.__handle_acquire)
+
         # TODO
 
     def __handle_acquire(self, change):
         def _msg():
             # TODO
+            if "connected" in change:
+                return None
+            if "error" in change:
+                return f"Error: {change.error}"
+            if change.value:
+                return "Acquiring"
             return "Idle"
 
         # TODO
+        if (m := _msg()) is None:
+            return
+        with self.lock_for_update() as txn:
+            txn.field_value_set("status", m)
         pass
 
 
