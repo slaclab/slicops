3f2d5607f0c05b7120512523faf0db949ea805b2
William Colocho <colocho@stanford.edu>
2026-03-18 11:47:29 -0700

    empty device_sql_db_test

---
+++ tests/device_sql_db_test.py
@@ -0,0 +1,14 @@
+"""Test device_sql_db
+
+:copyright: Copyright (c) 2026 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+from pykern import pkunit, pkio, pkdebug
+from pykern.pkcollections import PKDict
+
+
+def test_upstream_screens():
+    from slicops import device_sql_db
+
+    # TODO
