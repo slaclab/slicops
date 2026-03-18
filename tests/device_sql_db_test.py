d1e52e02835670e855a59eee7ce27053e4de5fbd
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-03-18 23:26:22 +0000

    20260318 database final

---
+++ tests/device_sql_db_test.py
@@ -0,0 +1,18 @@
+"""Test device_sql_db
+
+:copyright: Copyright (c) 2026 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:license: http://github.com/slaclab/slicops/LICENSE
+"""
+
+from pykern import pkunit, pkio, pkdebug
+from pykern.pkcollections import PKDict
+
+
+def test_upstream_screens():
+    from pykern import pkdebug, pkunit
+    from slicops import device_sql_db
+
+    r = device_sql_db.upstream_screens("CU_HXR", "OTR3")
+    pkunit.pkeq("YAG01", r[0], "r={}", r)
+    pkunit.pkeq(7, len(r))
+    pkunit.pkok("OTR3" not in r, "OTR3 in r={}", r)
