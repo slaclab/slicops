3dac71618d1df527e2f10b23538895fc6a52d1cf
Shamin Chowdhury <shamin@slac.stanford.edu>
2026-03-18 13:37:30 -0700

    empty device_sql_db_test

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -4,6 +4,9 @@
 :license: http://github.com/slaclab/slicops/LICENSE
 """
 
+import queue
+import threading
+
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 import slicops.sliclet
@@ -15,20 +18,25 @@ class Hello(slicops.sliclet.Base):
     def handle_destroy(self):
         if self.__device:
             self.__device.destroy()
+        self.__acquire_q.put_nowait(None)
 
     def handle_init(self, txn):
         self.__device = None
+        self.__acquire_q = queue.Queue()
 
     # called in thread so we can do "work" like create device
     def handle_start(self, txn):
         self.__device = slicops.device.Device("DEV_CAMERA")
+        self.__acquire_thread = threading.Thread(
+            target=self.__acquire_worker, daemon=True
+        )
+        self.__acquire_thread.start()
         self.__device.accessor("acquire").monitor(
             self.__handle_acquire
         )
 
-    # monitor callback happens in an EPICS thread
-    def __handle_acquire(self, change):
-        def _msg():
+    def __acquire_worker(self):
+        def _msg(change):
             if "connected" in change:
                 return "Connected"
             if "error" in change:
@@ -36,9 +44,14 @@ class Hello(slicops.sliclet.Base):
             if change.value:
                 return "Acquiring"
             return "Idle"
-
-        with self.lock_for_update() as txn:
-            txn.field_value_set("status", _msg())
+        
+        while (w := self.__acquire_q.get()) is not None:
+            with self.lock_for_update() as txn:
+                txn.field_value_set("status", _msg(w))
+    
+    # monitor callback happens in an EPICS thread
+    def __handle_acquire(self, change):
+        self.__acquire_q.put_nowait(change)
 
 
 CLASS = Hello
