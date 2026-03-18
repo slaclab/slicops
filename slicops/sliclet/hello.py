ddba821a10ad8e62252db26202eda67ef8684d4f
Nicole Neveu <nneveu@slac.stanford.edu>
2026-03-18 13:38:36 -0700

    empty device_sql_db_test

--- slicops/sliclet/hello.py
+++ slicops/sliclet/hello.py
@@ -8,25 +8,31 @@ from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 import slicops.sliclet
 import slicops.device
-
+import threading
+import queue
 
 class Hello(slicops.sliclet.Base):
 
     def handle_destroy(self):
         if self.__device:
             self.__device.destroy()
+        self.__acquire_q.put_nowait(None)
 
     def handle_init(self, txn):
         self.__device = None
+        self.__acquire_q = queue.Queue()
 
     def handle_start(self, txn):
         # in sliclet thread - manages work, you shouldn't have to lock, has transaction (txn)
         # in here we're already in lock
         self.__device = slicops.device.Device("DEV_CAMERA")
+        self.__acquire_thread = threading.Thread(
+                target=self.__acquire_worker, daemon=True)
+        self.__acquire_thread.start()
         self.__device.accessor("acquire").monitor(self.__handle_acquire)
 
-    def __handle_acquire(self, change):
-        def _msg():
+    def __acquire_worker(self):
+        def _msg(change):
             # no txn, external callback, outside sliclet thread, should not do any work,
             # should use a queue only
             if "connected" in change:
@@ -37,11 +43,13 @@ class Hello(slicops.sliclet.Base):
                 return "Acquiring"
             return "Idle"
 
-        if (m:= _msg()) is None:
-            return
+        while(w:= self.__acquire_q.get()) is not None:
+            with self.lock_for_update() as txn:
+                txn.field_value_set("status", _msg(w))
 
-        with self.lock_for_update() as txn:
-            txn.field_value_set("status", m)
+    def __handle_acquire(self, change):
+        self.__acquire_q.put_nowait(change)
+
 
 
 CLASS = Hello
